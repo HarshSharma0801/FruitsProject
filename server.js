0b250e38c42dc24bef9b00def73f676b99d7b1ca
HarshSharma0801 <121893196+HarshSharma0801@users.noreply.github.com>
2023-07-14 01:57:03 +0530

    update

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const Router = require("./routes")
 const app = express();
 
 app.use(express.json());
@@ -10,16 +11,17 @@ mongoose.connect('mongodb+srv://Harsh:EcP7XcFo03xbJQvy@test.gd2ldlk.mongodb.net/
   }
 );
 const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error: "));
+db.on("error",function () {
+  console.log("Error");
+}); 
 db.once("open", function () {
   console.log("Connected successfully");
 });
-app.get('/',(req,res)=>{
-    console.log('Made it');
-})
+app.use(Router);
 app.listen(27017, () => {
     console.log("Server is running at port 27017");
   });
+
 
 
 
